9c256c3bf3fd96c73907a50fb1e6e57709c2eaa6
hardy-navadiya <hardynavadiya.tech@gmail.com>
2022-01-05 15:01:11 +0530

    student valdiations

---
+++ app/models/student.rb
@@ -0,0 +1,9 @@
+class Student < ApplicationRecord
+	before_save { self.email = email.downcase }
+	regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :name, presence: true, length: { maximum: 50 }
+	validates :email, presence: true, length: { maximum: 255 }, format: { with: regex }, uniqueness: { case_sensitive: false}
+
+	has_secure_password
+	validates :password, presence: true, length: { minimum: 3 }
+end
